ba331e9fc4127cc38fdecb19e9409023c6decb96
Renato Araujo <orenatoaraujo@hotmail.com>
2018-11-10 10:55:22 -0200

    Adicionando o localStorage

--- src/app/dados-demograficos/dados-demograficos.component.ts
+++ src/app/dados-demograficos/dados-demograficos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Sexo } from '../sexo.enum';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-dados-demograficos',
@@ -8,8 +9,8 @@ import { Sexo } from '../sexo.enum';
 })
 export class DadosDemograficosComponent implements OnInit {
 
-  constructor() {
-    
+  constructor(private database:DatabaseService) {
+    console.log(this.database.findById('test', 'a589c02a-7a45-4f8b-a59e-acfdad3e8d46'));
   }
 
   ngOnInit() {
